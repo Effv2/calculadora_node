be16701c3583f7b7b4ec334e5a3a1231aa751ab9
Effv2 <frentyv2.0@gmail.com>
2019-02-08 20:52:49 -0600

    Calculadora con operaciones básicas

---
+++ index.js
@@ -0,0 +1,5 @@
+const math = require('./math');
+console.log(math.suma(30, 20));
+console.log(math.resta(30, 20));
+console.log(math.mult(30, 20));
+console.log(math.division(30, 20));
---
+++ math.js
@@ -0,0 +1,20 @@
+
+//function suma(a, b){
+ //   a+b;
+//    return a + b;
+//}
+
+module.exports = {
+suma : function (a, b){
+    return a + b;
+},
+resta : function (a, b){
+    return a - b;
+},
+mult : function (a, b){
+    return a * b;
+},
+division : function (a, b){
+    return a / b;
+}
+}
